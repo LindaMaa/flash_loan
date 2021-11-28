1225166e5df331a40482c4d3b9a10beaba8ffe12
Linda Mazanova <52823249+LindaMaa@users.noreply.github.com>
2021-11-28 06:27:06 +0000

    Create ILendingPoolAddressesProvider.sol

---
+++ ILendingPoolAddressesProvider.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.6.6;
+/**
+    @title ILendingPoolAddressesProvider interface
+    @notice provides the interface to fetch the LendingPoolCore address
+ */
+
+interface ILendingPoolAddressesProviderV1 {
+    function getLendingPoolCore() external view returns (address payable);
+    function getLendingPool() external view returns (address);
+}
